6aa28b55084205ade9b72ebd17e1551f6d703ee7
mabuta <mabutamail@gmail.com>
2019-05-21 13:29:02 +0300

    work

---
+++ src/Chapter10/WriteDemo.java
@@ -0,0 +1,13 @@
+package Chapter10;
+
+
+// Demonstrate System.out.write().
+class WriteDemo {
+    public static void main(String args[]) {
+        int b;
+
+        b = 'X';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
